bfe18ad60d1fb07d039e6d776b25d4fc74a52390
ReshnaRaj <reshnakripa2884@gmail.com>
2025-07-28 16:42:22 +0530

    added task schema

---
+++ server/src/models/task.mode.ts
@@ -0,0 +1,69 @@
+import mongoose, { Document, Schema, Types } from "mongoose";
+export interface ITask extends Document {
+  title: string;
+  description: string;
+  priority: "low" | "medium" | "high";
+  status: "open" | "in-progress" | "completed";
+  dueDate: Date;
+  assignedTo: mongoose.Types.ObjectId;
+  createdBy: mongoose.Types.ObjectId;
+  attachments: string[];
+  comments: IComment[];
+}
+export interface IComment extends Document {
+  user: mongoose.Types.ObjectId;
+  text: string;
+  createdAt: Date;
+}
+const taskSchema = new Schema<ITask>({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  priority: {
+    type: String,
+    enum: ["low", "high", "medium"],
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["completed", "open", "in-progres"],
+  },
+  dueDate: {
+    type: Date,
+    required: true,
+  },
+  assignedTo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  attachments: {
+    type: [String],
+  },
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+      text: {
+        type: String,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+});
+export const TaskModel=mongoose.model<ITask>("Task",taskSchema)
