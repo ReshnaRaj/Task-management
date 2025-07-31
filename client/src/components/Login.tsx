391a2a07ae872691f988b8faf69f94d90df51daf
ReshnaRaj <reshnakripa2884@gmail.com>
2025-07-31 20:33:15 +0530

    login and signup design completed

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,6 +1,8 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import login from "@/assets/login.png";
+import { LockKeyhole } from "lucide-react";
+import { LockKeyholeOpen } from "lucide-react";
 
 const Login = () => {
   return (
@@ -9,59 +11,80 @@ const Login = () => {
         <header className="h-16 flex items-center font-black text-2xl ml-8 mt-10 ">
           Taskify
         </header>
-       
+
         <div className="flex flex-col md:flex-row flex-1 overflow-hidden">
           <div
             className="hidden md:flex md:w-1/2 items-center justify-center bg-no-repeat"
             style={{ backgroundImage: `url(${login})` }}
           ></div>
 
-          <div className="w-full md:w-1/2 flex items-start justify-center py-10 px-6 md:p-16">
-            <div className="w-full max-w-lg p-8 md:p-16 mt-2">
-              <h2 className="flex   justify-center text-3xl font-bold mb-9">
-                Login
-              </h2>
-              <form className="flex flex-col gap-4">
-                <input
-                  type="email"
-                  placeholder="Email"
-                  className="border p-3 rounded outline-none w-full"
-                />
-                <input
-                  type="password"
-                  placeholder="Password"
-                  className="border p-3 rounded outline-none w-full"
-                />
-                <div className="flex justify-between text-sm">
-                  <span></span>
-                  <a href="#" className="text-blue-600 hover:underline">
-                    Forgot password?
-                  </a>
-                </div>
-                <button className="bg-black text-white p-3 rounded hover:bg-gray-800 transition">
+          <div className="w-full md:w-1/2 flex items-start justify-center py-10 px-6 md:p-16  ">
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                transition: { duration: 0.6, ease: "easeOut" },
+              }}
+              className="w-full max-w-lg md:-mt-20"
+            >
+              <div className="w-full max-w-lg p-8 md:p-16 mt-2">
+                <h2 className="flex   justify-center text-3xl font-bold mb-9">
                   Login
+                </h2>
+                <form className="flex flex-col gap-4">
+                  <input
+                    type="email"
+                    placeholder="Enter Email"
+                    className="border p-3 rounded outline-none w-full"
+                  />
+                  <div className="relative">
+                    <input
+                      type="password"
+                      placeholder="Enter Password"
+                      className="border p-3 rounded outline-none w-full"
+                    />
+                    <span className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer">
+                      <LockKeyhole className="w-5 h-5" />
+                    </span>
+                  </div>
+
+                  
+                  <div className="flex justify-between text-sm">
+                    <span>Having trouble signing in?</span>
+                    <a href="#" className="text-blue-600 hover:underline">
+                      Forgot password?
+                    </a>
+                  </div>
+                  <button className="bg-black text-white p-3 rounded hover:bg-gray-800 transition">
+                    Login
+                  </button>
+                </form>
+                <div className="flex items-center gap-2 my-6">
+                  <hr className="flex-1 border-gray-300" />
+                  <span className="text-sm text-gray-500">OR</span>
+                  <hr className="flex-1 border-gray-300" />
+                </div>
+                <button className="flex items-center justify-center border p-3 rounded w-full hover:bg-gray-100 transition cursor-pointer">
+                  <img
+                    src="https://www.svgrepo.com/show/475656/google-color.svg"
+                    alt="Google"
+                    className="w-5 h-5 mr-2"
+                  />
+                  Continue with Google
                 </button>
-              </form>
-              <div className="flex items-center gap-2 my-6">
-                <hr className="flex-1 border-gray-300" />
-                <span className="text-sm text-gray-500">OR</span>
-                <hr className="flex-1 border-gray-300" />
+                <p className="mt-6 text-sm text-center">
+                  Don’t have an account?{" "}
+                  <Link
+                    to="/register"
+                    className="text-[#0077B6] dark:text-[#00FFE5] font-medium hover:underline"
+                  >
+                    {" "}
+                    Sign up
+                  </Link>
+                </p>
               </div>
-              <button className="flex items-center justify-center border p-3 rounded w-full hover:bg-gray-100 transition cursor-pointer">
-                <img
-                  src="https://www.svgrepo.com/show/475656/google-color.svg"
-                  alt="Google"
-                  className="w-5 h-5 mr-2"
-                />
-                Continue with Google
-              </button>
-              <p className="mt-6 text-sm text-center">
-                Don’t have an account?{" "}
-                <a href="#" className="text-blue-600 hover:underline">
-                  Sign up
-                </a>
-              </p>
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -1,9 +1,101 @@
-import React from 'react'
-
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import login from "@/assets/login.png";
+import { LockKeyhole } from "lucide-react";
+import { LockKeyholeOpen } from "lucide-react";
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <>
+      <div className="h-screen flex flex-col overflow-hidden">
+        <header className="h-16 flex items-center font-black text-2xl ml-8 mt-10 ">
+          Taskify
+        </header>
 
-export default Signup
+        <div className="flex flex-col md:flex-row flex-1 overflow-hidden">
+          <div
+            className="hidden md:flex md:w-1/2 items-center justify-center bg-no-repeat"
+            style={{ backgroundImage: `url(${login})` }}
+          ></div>
+
+          <div className="w-full md:w-1/2 flex items-start justify-center py-10 px-6 md:p-16  ">
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                transition: { duration: 0.6, ease: "easeOut" },
+              }}
+              className="w-full max-w-lg md:-mt-20"
+            >
+              <div className="w-full max-w-lg p-8 md:p-16 mt-2">
+                <h2 className="flex   justify-center text-3xl font-bold mb-9">
+                  Sign Up
+                </h2>
+                <form className="flex flex-col gap-4">
+                  <input
+                    type="name"
+                    placeholder="Enter Name"
+                    className="border p-3 rounded outline-none w-full"
+                  />
+                  <input
+                    type="email"
+                    placeholder="Enter Email"
+                    className="border p-3 rounded outline-none w-full"
+                  />
+
+                  <div className="relative">
+                    <input
+                      type="password"
+                      placeholder="Enter Password"
+                      className="border p-3 rounded outline-none w-full"
+                    />
+                    <span className="absolute  right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer">
+                      <LockKeyhole className="w-5 h-5" />
+                    </span>
+                  </div>
+                   <div className="relative">
+                    <input
+                      type="password"
+                      placeholder="Enter Password again"
+                      className="border p-3 rounded outline-none w-full"
+                    />
+                    <span className="absolute  right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer">
+                      <LockKeyhole className="w-5 h-5" />
+                    </span>
+                  </div>
+                  <div className="flex justify-between text-sm">
+                    <span>Having trouble signing in?</span>
+                    <a href="#" className="text-blue-600 hover:underline">
+                      Forgot password?
+                    </a>
+                  </div>
+                  <button className="bg-black text-white p-3 rounded hover:bg-gray-800 transition">
+                    Sign Up
+                  </button>
+                </form>
+                <div className="flex items-center gap-2 my-6">
+                  <hr className="flex-1 border-gray-300" />
+                  <span className="text-sm text-gray-500">OR</span>
+                  <hr className="flex-1 border-gray-300" />
+                </div>
+
+                <p className="mt-6 text-sm text-center">
+                  Already have a account?{" "}
+                  <Link
+                    to="/"
+                    className="text-[#0077B6] dark:text-[#00FFE5] font-medium hover:underline"
+                  >
+                    {" "}
+                    Login
+                  </Link>
+                </p>
+              </div>
+            </motion.div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
